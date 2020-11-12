2bd2c432f625d1d154f8d88cc2e44db045d15ab7
jack5341 <nedim.akar53411@gmail.com>
2020-11-12 23:35:49 +0100

    feat: exported from other folder

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-var x = 0
-
-setInterval(() => {
-    x += 1
-    document.querySelector("#cpuProgress").value = x
-    document.querySelector("#cpuSmall").innerHTML = x + "%"
-    document.querySelector("#memProgress").value = x
-    document.querySelector("#memSmall").innerHTML = x + "%"
-}, 100);
